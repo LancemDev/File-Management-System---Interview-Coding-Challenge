fbac26368f7dc295889377c13549c0f69c1ad258
LancemDev <munyaolance1@gmail.com>
2024-12-20 12:14:44 +0300

    :zap: Feat: created the model and migration files and added the functions for controlers

--- backend/app/Http/Controllers/DirectoryController.php
+++ backend/app/Http/Controllers/DirectoryController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Directory;
 use Illuminate\Http\Request;
 
 class DirectoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return Directory::all();
+    }
+
+    public function getSubDirectories($id)
+    {
+        return Directory::where('parent_id', $id)->get();
+    }
+
+    public function getFiles($id)
+    {
+        return Directory::findOrFail($id)->files;
+    }
+
+    public function store(Request $request)
+    {
+        $directory = new Directory();
+        $directory->name = $request->name;
+        $directory->parent_id = $request->parent_id;
+        $directory->save();
+
+        return response()->json($directory, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $directory = Directory::findOrFail($id);
+        $directory->name = $request->name;
+        $directory->parent_id = $request->parent_id;
+        $directory->save();
+
+        return response()->json($directory, 200);
+    }
+
+    public function destroy($id)
+    {
+        $directory = Directory::findOrFail($id);
+        $directory->delete();
+
+        return response()->json(null, 204);
+    }
 }
--- backend/app/Http/Controllers/FileController.php
+++ backend/app/Http/Controllers/FileController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\File;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+    public function index()
+    {
+        return File::all();
+    }
+
+    public function show($id)
+    {
+        return File::findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        $file = new File();
+        $file->name = $request->name;
+        $file->path = $request->path;
+        $file->directory_id = $request->directory_id;
+        $file->save();
+
+        return response()->json($file, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $file = File::findOrFail($id);
+        $file->name = $request->name;
+        $file->path = $request->path;
+        $file->directory_id = $request->directory_id;
+        $file->save();
+
+        return response()->json($file, 200);
+    }
+
+    public function destroy($id)
+    {
+        $file = File::findOrFail($id);
+        $file->delete();
+
+        return response()->json(null, 204);
+    }
 }
