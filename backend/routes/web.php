f4bb772b55f01e4ace3246d7bc60c695ad6b2a4c
LancemDev <munyaolance1@gmail.com>
2024-12-20 12:12:19 +0300

    :zap: Feat: created the routes and controller files

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\DirectoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// File routes
+Route::get('/api/files', [FileController::class, 'index']);
+Route::get('/api/files/{id}', [FileController::class, 'show']);
+Route::post('/api/files', [FileController::class, 'store']);
+Route::put('/api/files/{id}', [FileController::class, 'update']);
+Route::delete('/api/files/{id}', [FileController::class, 'destroy']);
+
+// Directory routes
+Route::get('/api/directories', [DirectoryController::class, 'index']);
+Route::get('/api/directories/{id}/sub-directories', [DirectoryController::class, 'getSubDirectories']);
+Route::get('/api/directories/{id}/files', [DirectoryController::class, 'getFiles']);
+Route::post('/api/directories', [DirectoryController::class, 'store']);
+Route::put('/api/directories/{id}', [DirectoryController::class, 'update']);
+Route::delete('/api/directories/{id}', [DirectoryController::class, 'destroy']);
+
