9f1fb69c5ecf872807ffd043e8880a54d83a5aa7
LancemDev <munyaolance1@gmail.com>
2024-12-21 10:32:51 +0300

    :zap: Update: routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,22 +9,17 @@ Route::get('/', function () {
 });
 
 // File routes
-Route::get('/api/files', [FileController::class, 'index']);
-Route::get('/api/files/{id}', [FileController::class, 'show']);
+Route::get('/api/files', [FileController::class, 'index'])->name('api.files');
+Route::get('/api/files/{id}', [FileController::class, 'show'])->name('api.files.show');
 Route::post('/api/files', [FileController::class, 'store'])->name('api.files');
-Route::put('/api/files/{id}', [FileController::class, 'update']);
-Route::delete('/api/files/{id}', [FileController::class, 'destroy']);
+Route::put('/api/files/{id}', [FileController::class, 'update'])->name('api.files');
+Route::delete('/api/files/{id}', [FileController::class, 'destroy'])->name('api.files');
 
 // Directory routes
-Route::get('/api/directories', [DirectoryController::class, 'index']);
-Route::get('/api/directories/{id}/sub-directories', [DirectoryController::class, 'getSubDirectories']);
-Route::get('/api/directories/{id}/files', [DirectoryController::class, 'getFiles']);
-Route::post('/api/directories', [DirectoryController::class, 'store']);
-Route::put('/api/directories/{id}', [DirectoryController::class, 'update']);
-Route::delete('/api/directories/{id}', [DirectoryController::class, 'destroy']);
-Route::get('/files{id}/download', [FileController::class, 'download']);
-
-
-Route::get('/test-upload', function(){
-    return view('upload');
-});
+Route::get('/api/directories', [DirectoryController::class, 'index'])->name('api.directories');
+Route::get('/api/directories/{id}/sub-directories', [DirectoryController::class, 'getSubDirectories'])->name('api.directories.sub-directories');
+Route::get('/api/directories/{id}/files', [DirectoryController::class, 'getFiles'])->name('api.directories.files');
+Route::post('/api/directories', [DirectoryController::class, 'store'])->name('api.directories');
+Route::put('/api/directories/{id}', [DirectoryController::class, 'update'])->name('api.directories');
+Route::delete('/api/directories/{id}', [DirectoryController::class, 'destroy'])->name('api.directories');
+Route::get('/files{id}/download', [FileController::class, 'download'])->name('files.download');
